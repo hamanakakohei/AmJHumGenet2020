c72be3563eb0b7154e786dc64fe0d9544714837f
hamanakakohei <46100957+hamanakakohei@users.noreply.github.com>
2019-11-21 21:53:03 +0900

    Update fig.1f.R

--- fig.1f.R
+++ fig.1f.R
@@ -1,4 +1,35 @@
 ## depth file -> bedgraph file ##
+library(tidyverse)
+depth_path = "DepthV50417M.txt"
+OUT = "v5.bedgraph"
+tmp_v4 = read_tsv(depth_path,col_names=c("chr","pos","v4"),col_types="cdd")
+
+## depth filt => bedgraph format ##
+v4 = filter(tmp_v4,chr=="chr19" & pos>4500000 & pos<4600000)
+
+sta_ary <- c(v4$pos[1])
+end_ary <- c()
+val_ary <- c(v4$v4[1])
+for(i in 2:nrow(v4)){
+    if(v4$pos[i]!=v4$pos[i-1]+1){
+        end <- v4$pos[i-1]
+        sta <- v4$pos[i] - 1
+        val <- v4$v4[i]
+        end_ary <- c(end_ary,end,sta)       
+        sta_ary <- c(sta_ary,end,sta)       
+        val_ary <- c(val_ary,0,v4$v4[i])  
+    } else if(v4$pos[i]==v4$pos[i-1]+1 && v4$v4[i]!=v4$v4[i-1]){
+        end <- v4$pos[i-1]
+        sta <- v4$pos[i] - 1
+        val <- v4$v4[i]
+        end_ary <- c(end_ary,end)
+        sta_ary <- c(sta_ary,sta)
+        val_ary <- c(val_ary,v4$v4[i])
+    }
+}
+end_ary <- c(end_ary, v4$pos[nrow(v4)])
+df <- data.frame(chr="chr19",start=sta_ary,end=end_ary,value=val_ary)
+write_tsv(df,OUT,quote=F)
 
 
 ## gviz plot ##
